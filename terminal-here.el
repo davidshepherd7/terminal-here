e975234095fb8e4dbcf14c031b94e4d139e91490
David Shepherd <davidshepherd7@gmail.com>
2020-06-13 16:49:22 +0000

    Increase emacs version minimum as suggested by package lint

--- terminal-here.el
+++ terminal-here.el
@@ -4,7 +4,7 @@
 
 ;; Author: David Shepherd <davidshepherd7@gmail.com>
 ;; Version: 1.0
-;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
+;; Package-Requires: ((emacs "24.3") (cl-lib "0.5"))
 ;; Keywords: tools, frames
 ;; URL: https://github.com/davidshepherd7/terminal-here
 
